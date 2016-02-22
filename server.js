245d5f12fc3169c532d75c345b145bf6b0ea2558
logan dougher <logan.dougher@gmail.com>
2016-02-22 11:16:55 -0600

    added register view, installed redis and session to track user and # of visits or req per path

--- server.js
+++ server.js
@@ -3,14 +3,28 @@
 const express = require('express');
 const chalk = require('chalk');
 const bodyParser = require('body-parser');
-
+const session = require('express-session');
+const RedisStore = require('connect-redis')(session);
 const PORT = process.env.PORT || 3000;
-
+const SESSION_SECRET = process.env.SESSION_SECRET || 'logansecret';
 const app = express();
 
 app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(session({
+  secret: SESSION_SECRET,
+  store: new RedisStore()
+}));
+
+app.use((req, res, next) => {
+  req.session.visits = req.session.visits || {};
+  req.session.visits[req.url] = req.session.visits[req.url] || 0;
+  req.session.visits[req.url]++ // logs the # of req or visits per path
+  console.log(req.session);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.render('index');
@@ -22,6 +36,21 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
   res.redirect('/');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.post('/register', (req, res) => {
+  if(req.body.password === req.body.verify) {
+    res.redirect('/login');
+  } else {
+    res.render('register', {
+      email: req.body.email,
+      message: 'passwords do not match'
+    });
+  }
 });
 
 app.listen(PORT, () => {
